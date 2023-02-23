6cf4aff27ec6d2f4de5bd79657a216b62cb984ec
santoshreddyy <108527128+santoshreddyy@users.noreply.github.com>
2023-02-23 16:32:02 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to DevOps CICD using jenkins and pm2-pm2");
+    response.end("Hello, Welcome to DevOps CICD using jenkins and pm2-pm2-pm2");
 
 });
 
