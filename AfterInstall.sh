8a7eb612006fb9a9dc2603f104d4c9eec89c3369
santoshreddyy <108527128+santoshreddyy@users.noreply.github.com>
2023-02-23 12:43:11 +0530

    Create AfterInstall.sh

---
+++ AfterInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+cd /home/ubuntu/api
+npm install
+pm2 start /home/ubuntu/api/ecosystem.config.js
